a2f987260fac5cd5c20e7ffc48c7c9803c45c68c
ZAHN Hugo <hugo.zahn@soprasteria.com>
2019-06-11 14:40:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from process import process
+from confluent_kafka import Consumer
+from confluent_kafka import KafkaError
+from elasticsearch import Elasticsearch
+import sys
+
+if __name__ == "__main__":
+
+    topic_name = sys.argv[1]
+    consumer_group_id = sys.argv[2]
+    index_name = sys.argv[3]
+
+    print("topic_name : ", sys.argv[1])
+    print("consumer_group_id : ", sys.argv[2])
+    print("index_name : ", sys.argv[3])
+
+    es = Elasticsearch(hosts="localhost:9200")
+
+    consumer = Consumer({
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': consumer_group_id,
+        'default.topic.config': {'auto.offset.reset': 'smallest',
+                                 'enable.auto.commit': False}
+    })
+
+    consumer.subscribe(topic_name)
+
+    msgs = []
+    nb_messages = 0
+    Running = True
+
+    while Running:
+        msg = consumer.poll(-1)
+
+        if msg:
+            if not msg.error():
+                res = es.index(index=index_name, doc_type='_doc', body=process(msg.value()))
+                print(res['result'])
+
+            elif msg.error().code() != KafkaError._PARTITION_EOF:
+                print(msg.error())
+                Running = False
+
+    consumer.close()  # On ferme le consumer
---
+++ process.py
@@ -0,0 +1,28 @@
+import json
+import datetime
+
+
+def process(binary_data):
+    json_data = json.loads(binary_data)
+    data = filter_data(json_data)
+    return json.dumps(data)
+
+
+def filter_data(json_data):
+    data = {"text": json_data["extended_tweet"]["full_text"] if json_data['truncated'] else json_data["text"],
+            "created_at": int(datetime.datetime.strptime(json_data["created_at"], "%a %b %d %H:%M:%S %z %Y").timestamp()*1000),
+            "in_reply_to_user_id": json_data["in_reply_to_user_id"],
+            "in_reply_to_status_id": json_data["in_reply_to_status_id"], "coordinates": json_data["coordinates"],
+            "retweeted_status_id": json_data["retweeted_status"]["id"] if json_data["retweeted"] else None,
+            "user": {}, "entities": {}}
+
+    data["user"]["id"] = json_data["user"]["id"]
+    data["user"]["name"] = json_data["user"]["name"]
+    data["user"]["followers_count"] = json_data["user"]["followers_count"]
+    data["user"]["verified"] = json_data["user"]["verified"]
+
+    data["entities"]["urls"] = json_data["entities"]["urls"]
+    data["entities"]["hashtags"] = json_data["entities"]["hashtags"]
+    data["entities"]["user_mentions"] = json_data["entities"]["user_mentions"]
+
+    return data
